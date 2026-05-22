d37147b0fb6708a3ec93d2dde8a48306796ffa4b
ViniciusBFranca <146156694+ViniciusBFranca@users.noreply.github.com>
2026-05-22 17:53:16 -0300

    Finalizado

---
+++ scripts.js
@@ -0,0 +1,22 @@
+let body = document.querySelector("body")
+let tenis = document.querySelector(".imagem-tenis")
+let botao = document.querySelector(".botao-carrinho")
+// document = html
+// querySelector = pegador / selecionador
+
+function mudarVisual(cor, imagem) {
+    tenis.classList.add("troca-efeito")
+
+    body.style.background = cor
+
+    botao.style.background = cor
+
+
+    // contador de tempo
+    setTimeout(() => {
+        tenis.src = imagem
+        tenis.classList.remove("troca-efeito")
+    }, 500);
+
+    // 1000 milisegundos = 1segundo
+}
